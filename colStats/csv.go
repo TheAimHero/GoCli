bdfb95f8bb661950c7216733690d17a749df100c
TheAimHero <vsghodekar1@gmail.com>
2023-12-04 17:48:45 +0530

    basic stats calculating cli

---
+++ colStats/csv.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"encoding/csv"
+	"fmt"
+	"io"
+	"strconv"
+)
+
+type statsFunc func(data []float64) float64
+
+func sum(data []float64) float64 {
+	sum := 0.0
+	for _, v := range data {
+		sum += v
+	}
+	return sum
+}
+
+func avg(data []float64) float64 {
+	return sum(data) / float64(len(data))
+}
+
+func csv2float(r io.Reader, column int) ([]float64, error) {
+	cr := csv.NewReader(r)
+	cr.ReuseRecord = true
+	column--
+
+	var data []float64
+	for i := 0; ; i++ {
+		row, err := cr.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return nil, fmt.Errorf("cannot read data from file: %w", err)
+		}
+
+		if i == 0 {
+			continue
+		}
+
+		if len(row) <= column {
+			return nil, fmt.Errorf("%w: File has only %d columns", ErrInvalidColumn, len(row))
+		}
+
+		v, err := strconv.ParseFloat(row[column], 64)
+		if err != nil {
+			return nil, fmt.Errorf("%w: %s", ErrNotNumber, err)
+		}
+
+		data = append(data, v)
+	}
+	return data, nil
+}
---
+++ colStats/go.mod
@@ -0,0 +1,5 @@
+module colStats
+
+go 1.21.4
+
+require golang.org/x/tools v0.15.0 // indirect
